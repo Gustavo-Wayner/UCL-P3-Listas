cbcf4ff70f545d8e36cd9f07b67822ee21fca4c4
Gustavo-Wayner <gusgabway@gmail.com>
2026-02-16 12:49:17 -0300

    Adicionada a questão 4 da lista 1 nivel facil

---
+++ L1/Q04F.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Program;
+
+public static class Fibonacci
+{
+    public static string ToCommaNoation(int n)
+    {
+        string n_str = n.ToString();
+        int l = n_str.Length;
+
+        for ( int i = l - 3; i > 0; i -= 3 )
+        {
+            n_str = n_str.Insert(i, ".");
+        }
+
+        return n_str;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("0");
+        int f = 1;
+        int prev = 0;
+
+        for ( int i = 0; i < 30; i++ )
+        {
+            Console.WriteLine(ToCommaNoation(f));
+            f += prev;
+            prev = f - prev;
+        }
+    }
+}
