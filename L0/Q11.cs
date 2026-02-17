26f242163c3336dde0b1d8ba605618ea92167f49
Gustavo-Wayner <gusgabway@gmail.com>
2026-02-16 22:46:51 -0300

    Feita a questão 11 da lista 0

---
+++ L0/Q11.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Raiz;
+
+public static class Raiz
+{
+	public static double Parse( string prompt, string err )
+	{
+		while ( true )
+		{
+			Console.Write(prompt);
+			if ( double.TryParse( Console.ReadLine(), out double result ) && result >= 0 ) return result;
+			Console.WriteLine(err);
+		}
+	}
+
+	static double Sqrt(double n, double tolerance)
+	{
+		if (n < 0) throw new ArgumentException("Numero negativo");
+
+		double x = n * 0.5D;
+		double last;
+
+		do
+		{
+			last = x;
+			x = 0.5 * (x + n / x);
+		}
+		while (Math.Abs(x - last) > tolerance);
+
+		return x;
+	}
+
+	public static void Main()
+	{
+		double num = Parse("Digite o numero do qual quer ver a raiz quadrada: ", "Digite um NUMERO POITIVO!");
+		double margin = Parse("Digite a margem de erro tolerada: ", "Digite um NUMERO POITIVO!");
+
+		Console.WriteLine($"A raiz quadrada de {num} com margem de erro {margin} é: {Sqrt(num, margin)}");
+	}
+}
